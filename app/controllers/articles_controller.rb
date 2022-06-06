2404067d7aaa11f14ed47c7c459106ee44ed5679
Jorge Rodriguez <jarf4321@gmail.com>
2022-06-06 12:53:09 -0500

    Add model scope method recent & controller index response

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
   def index
-    articles = Article.all
+    articles = Article.recent # call in the scope -> recent (on article model)
     render json: serializer.new(articles), status: :ok  # 200
   end
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
   validates :title, :content, :slug, presence: true
   validates :slug, uniqueness: true
+
+  # To make queries to the DB
+  scope :recent, -> { order(created_at: :desc) } # Retrieve Recent articles first
 end
